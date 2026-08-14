6de4c0e6f19fd5edbc7644100a48c9f210173428
donoftime2018 <donoftime2018@gmail.com>
2026-08-14 01:07:31 -0400

    price query added to browse url

--- Server/app.js
+++ Server/app.js
@@ -147,7 +147,7 @@ async function scrapePages(keyword, totalPages=1) {
   return all;
 }
 
-async function getItems(keyword, priceCeil = undefined, priceFloor = undefined, newCond = false, includeDescription = false)
+async function getItems(keyword, priceCeil = undefined, priceFloor = undefined, sortPrice=undefined, newCond = false, includeDescription = false)
 {
     // console.log(process.env.PROD_APP_ID)
     // console.log(process.env.PROD_CERT_ID)
@@ -169,6 +169,22 @@ async function getItems(keyword, priceCeil = undefined, priceFloor = undefined, 
 
     console.log('Price Floor:', priceFloor)
     console.log('Price Ceiling:', priceCeil)
+    console.log("Sort Price:", sortPrice)
+
+    let sorting = undefined
+    if (sortPrice !== undefined)
+    {
+      if (sortPrice == 'asc')
+      {
+        sorting = 'price'
+      }
+      else
+      {
+        sorting='-price'
+      }
+    }
+
+    console.log("Sort Price:", sorting)
 
     const filters = [
       includeDescription ? "searchInDescription:true" : undefined,
@@ -179,7 +195,7 @@ async function getItems(keyword, priceCeil = undefined, priceFloor = undefined, 
     ]
 
     const items = await axios.get('https://api.ebay.com/buy/browse/v1/item_summary/search', {
-        params: { q: keyword, filter: filters.filter(f=>f!==undefined).join(',')},
+        params: { q: keyword, filter: filters.filter(f=>f!==undefined).join(','), sort: sorting},
         headers: {
             'Authorization': `Bearer ${oAuth.data.access_token}`,
             'Content-Type': 'application/json',
@@ -221,8 +237,9 @@ async function getItems(keyword, priceCeil = undefined, priceFloor = undefined, 
 app.post("/api/data", async (req, res) => {
   
     console.log('Request body:', req.body);
-    const { query, newCond, priceFloor, priceCeil, desc } = req.body
+    const { query, newCond, priceFloor, priceCeil, sortPrice, desc } = req.body
     console.log('Query:', query);
+    console.log("Sort Price:", sortPrice)
     console.log('New Condition:', newCond)
     console.log('Including description:', desc);
     console.log('Price Floor:', priceFloor)
@@ -237,7 +254,7 @@ app.post("/api/data", async (req, res) => {
     }
 
     try {
-        const items = await getItems(query, priceCeil, priceFloor, newCond, desc)/// await scrapePages(query, totalPages);
+        const items = await getItems(query, priceCeil, priceFloor, sortPrice, newCond, desc)/// await scrapePages(query, totalPages);
         // console.log(items);
         // console.log(items.length)
         // await scrapePages(query, totalPages)
